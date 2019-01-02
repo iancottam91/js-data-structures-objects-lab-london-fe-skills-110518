879a39bb43ea09efe4004c2b83373b95b34dd646
Ian Cottam <ian.cottam@bbc.co.uk>
2019-01-02 18:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your solution in this file!
 const driver = {}
 
+const driver = {
+  'apple' : 'green'
+}
+
+
+
 function updateDriverWithKeyAndValue(driver, key, value) {
  const newDriver = Object.assign({}, driver);
  newDriver[key] = value;
